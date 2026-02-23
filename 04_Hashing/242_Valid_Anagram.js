dd7c16135d6a9db2be2cb9ac38d8b7aa2b0d5be1
Srinivasa Y R <VFG3587@VFG-DC.LOCAL>
2026-02-23 15:07:54 +0530

    updated

--- 04_Hashing/242_Valid_Anagram.js
+++ 04_Hashing/242_Valid_Anagram.js
@@ -3,3 +3,36 @@
 // Sample Output: false
 
 // TODO: Implement solution here
+
+var isAnagram = function(s, t) {
+    if(s.length==0 && t.length==0){
+        return true
+    }
+    if(s.length!=t.length){
+        return false
+    }
+    let map=new Map;
+    for(let i=0;i<s.length;i++){
+        if(map.has(s[i])){
+            map.set(s[i],(map.get(s[i] || 0)+1));
+        }
+        else{
+            map.set(s[i],1)
+        }
+        if(map.has(t[i])){
+            map.set(t[i],(map.get(t[i])-1));
+        }
+        else{
+            map.set(t[i],-1)
+        }
+    }
+    for(let [key,val] of map){
+        if(val!=0){
+            return false
+        }
+    }
+    return true;
+};
+let s="rat"
+let t="car"
+console.log(isAnagram(s,t))
--- 04_Hashing/290_Word_Pattern.js
+++ 04_Hashing/290_Word_Pattern.js
@@ -3,3 +3,31 @@
 // Sample Output: true
 
 // TODO: Implement solution here
+
+var wordPattern = function(pattern, s) {
+    let map=new Map();
+    arr=s.split(" ");
+    if(arr.length!=pattern.length){
+        return false
+    }
+    if(new Set(pattern.split("")).size!=new Set(s.split(" ")).size){
+        return false
+    }
+    for(let i=0;i<pattern.length;i++){
+        if(map.has(pattern[i])){
+            if(map.get(pattern[i])!=arr[i]){
+                return false;
+            }
+        }
+        else{
+            map.set(pattern[i],arr[i])
+        }
+    }
+    return true
+};
+
+let pattern='abba';
+let s='dog dog dog dog'
+
+console.log(wordPattern(pattern,s));
+
